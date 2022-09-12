217ee802c5c0cc3d1ec02026dedf456f1ce23e31
Ahmed Hussein <wolktheline@yahoo.com>
2022-09-12 08:12:23 -0600

    getting the button from the DOM and assign an eventListenner to it

--- challenge.js
+++ challenge.js
@@ -1,1 +1,14 @@
 "use strict";
+
+let button = document.querySelector(".btn");
+let text = "";
+let inputs = [];
+
+button.addEventListener("click", function () {
+  text = document.querySelector(".textarea").value;
+  inputs = text.split(" ");
+
+  for (const x of inputs) {
+    console.log(x.replace("_", "").trim());
+  }
+});
