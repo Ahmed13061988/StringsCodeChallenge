929d213e6c242a0f51796177cd6642162d4ef846
Ahmed Hussein <wolktheline@yahoo.com>
2022-09-12 10:28:12 -0600

    done

--- challenge.js
+++ challenge.js
@@ -8,8 +8,23 @@ button.addEventListener("click", function () {
   text = document.querySelector(".textarea").value;
   inputs = text.split("\n");
 
-  for (const word of inputs) {
-    let words = word.toLowerCase();
-    console.log(words.replace("\n", " ").replace("_", "").trim());
+  for (const [i, word] of inputs.entries()) {
+    const [first, second] = word.toLowerCase().trim().split("_");
+
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    console.log(`${output.padEnd(20)}${"✅".repeat(i + 1)}`);
   }
+
+  //   for (let i = 0; i < inputs.length; i++) {
+  //     let word =
+  //       inputs[i]
+  //         .toLowerCase()
+  //         .replace("\n", " ")
+  //         .trim()
+  //         .replace("_", inputs[i]) + " ✅".repeat(`${i + 1} `);
+  //     console.log(word);
+  //   }
 });
